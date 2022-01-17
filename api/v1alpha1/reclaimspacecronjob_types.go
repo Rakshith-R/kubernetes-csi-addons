8197e7380c6a533d686b56c8f51b27c52f63acea
Rakshith R <rar@redhat.com>
2022-01-17 10:47:10 +0530

    api: modify reclaimspacecronjob api
    Signed-off-by: Rakshith R <rar@redhat.com>

--- api/v1alpha1/reclaimspacecronjob_types.go
+++ api/v1alpha1/reclaimspacecronjob_types.go
@@ -17,36 +17,108 @@ limitations under the License.
 package v1alpha1
 
 import (
+	v1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
-// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
-// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
+// ConcurrencyPolicy describes how the job will be handled.
+// Only one of the following concurrent policies may be specified.
+// If none of the following policies is specified, the default one
+// is ForbidConcurrent.
+type ConcurrencyPolicy string
+
+const (
+	// ForbidConcurrent forbids concurrent runs, skipping next run if previous
+	// hasn't finished yet.
+	ForbidConcurrent ConcurrencyPolicy = "Forbid"
+
+	// ReplaceConcurrent cancels currently running job and replaces it with a new one.
+	ReplaceConcurrent ConcurrencyPolicy = "Replace"
+)
+
+// ReclaimSpaceJobTemplateSpec describes the data a Job should have when created from a template
+type ReclaimSpaceJobTemplateSpec struct {
+	// Standard object's metadata of the jobs created from this template.
+	// +kubebuilder:validation:Optional
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	// Specification of the desired behavior of the reclaimspacejob.
+	// +kubebuilder:validation:Required
+	Spec ReclaimSpaceJobSpec `json:"spec,omitempty"`
+}
 
 // ReclaimSpaceCronJobSpec defines the desired state of ReclaimSpaceCronJob
 type ReclaimSpaceCronJobSpec struct {
-	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
-	// Important: Run "make" to regenerate code after modifying this file
 
-	// Foo is an example field of ReclaimSpaceCronJob. Edit reclaimspacecronjob_types.go to remove/update
-	Foo string `json:"foo,omitempty"`
+	// The schedule in Cron format, see https://en.wikipedia.org/wiki/Cron.
+	// +kubebuilder:validation:Required
+	Schedule string `json:"schedule"`
+
+	// Optional deadline in seconds for starting the job if it misses scheduled
+	// time for any reason.  Missed jobs executions will be counted as failed ones.
+	// +kubebuilder:validation:Optional
+	StartingDeadlineSeconds *int64 `json:"startingDeadlineSeconds,omitempty"`
+
+	// Specifies how to treat concurrent executions of a Job.
+	// Valid values are:
+	// - "Forbid" (default): forbids concurrent runs, skipping next run if previous run hasn't finished yet;
+	// - "Replace": cancels currently running job and replaces it with a new one
+	// +kubebuilder:validation:Optional
+	ConcurrencyPolicy ConcurrencyPolicy `json:"concurrencyPolicy,omitempty"`
+
+	// This flag tells the controller to suspend subsequent executions, it does
+	// not apply to already started executions.  Defaults to false.
+	// +kubebuilder:validation:Optional
+	Suspend *bool `json:"suspend,omitempty"`
+
+	// Specifies the reclaimspacejob that will be created when executing a CronJob.
+	// +kubebuilder:validation:Required
+	ReclaimSpaceJobTemplate ReclaimSpaceJobTemplateSpec `json:"reclaimSpaceJobTemplate"`
+
+	// The number of successful finished jobs to retain. Value must be non-negative integer.
+	// Defaults to 3.
+	// +kubebuilder:validation:Optional
+	// +kubebuilder:validation:Maximum=60
+	// +kubebuilder:validation:Minimum=0
+	// +kubebuilder:default:=3
+	SuccessfulJobsHistoryLimit *int32 `json:"successfulJobsHistoryLimit,omitempty"`
+
+	// The number of failed finished jobs to retain. Value must be non-negative integer.
+	// Defaults to 1.
+	// +kubebuilder:validation:Optional
+	// +kubebuilder:validation:Maximum=60
+	// +kubebuilder:validation:Minimum=0
+	// +kubebuilder:default:=1
+	FailedJobsHistoryLimit *int32 `json:"failedJobsHistoryLimit,omitempty"`
 }
 
 // ReclaimSpaceCronJobStatus defines the observed state of ReclaimSpaceCronJob
 type ReclaimSpaceCronJobStatus struct {
-	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
-	// Important: Run "make" to regenerate code after modifying this file
+	// Active contains a pointer to currently running reclaimspacejob.
+	Active *v1.ObjectReference `json:"active,omitempty"`
+
+	// Information when was the last time the job was successfully scheduled.
+	LastScheduleTime *metav1.Time `json:"lastScheduleTime,omitempty"`
+
+	// Information when was the last time the job successfully completed.
+	LastSuccessfulTime *metav1.Time `json:"lastSuccessfulTime,omitempty"`
 }
 
 //+kubebuilder:object:root=true
 //+kubebuilder:subresource:status
+//+kubebuilder:printcolumn:JSONPath=".spec.schedule",name=Schedule,type=string
+//+kubebuilder:printcolumn:JSONPath=".spec.suspend",name=Suspend,type=boolean
+//+kubebuilder:printcolumn:JSONPath=".status.active.name",name=Active,type=string
+//+kubebuilder:printcolumn:JSONPath=".status.lastScheduleTime",name=Lastschedule,type=date
+//+kubebuilder:printcolumn:JSONPath=".metadata.creationTimestamp",name=Age,type=date
 
 // ReclaimSpaceCronJob is the Schema for the reclaimspacecronjobs API
 type ReclaimSpaceCronJob struct {
 	metav1.TypeMeta   `json:",inline"`
 	metav1.ObjectMeta `json:"metadata,omitempty"`
 
-	Spec   ReclaimSpaceCronJobSpec   `json:"spec,omitempty"`
+	// +kubebuilder:validation:Required
+	Spec   ReclaimSpaceCronJobSpec   `json:"spec"`
 	Status ReclaimSpaceCronJobStatus `json:"status,omitempty"`
 }
 
